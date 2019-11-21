9df29c5b157351d3c6095fa8f1b7548412f3678b
Kia Armani <contact@xi4.me>
2019-11-21 09:50:30 +0100

    Upgrade to .NET Framework 4.7.2, Code Cleanup.

--- Program.cs
+++ Program.cs
@@ -1,175 +1,181 @@
-﻿using CommandLine;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading;
+using CommandLine;
+using Newtonsoft.Json.Linq;
 
-namespace WoWUE4CmdImport
+namespace UnrealBatchImporter
 {
-    class Options
+    internal class Options
     {
         [Option('u', "ue4", Required = true,
-          HelpText = "Path to UE4 Win64 Binaries.")]
-        public String UE4Path { get; set; }
+            HelpText = "Path to UE4 Win64 Binaries.")]
+        public string UE4Path { get; set; }
 
         [Option('p', "project", Required = true,
-          HelpText = "Path to UE4 project.")]
-        public String ProjectPath { get; set; }
+            HelpText = "Path to UE4 project.")]
+        public string ProjectPath { get; set; }
 
         [Option('s', "source", Required = true,
-          HelpText = "Path to folder with source files.")]
-        public String SourceFilesPath { get; set; }
+            HelpText = "Path to folder with source files.")]
+        public string SourceFilesPath { get; set; }
 
         [Option('i', "importto", Required = true,
-          HelpText = "UE4 path to where files should get imported to")]
-        public String ImportedFilesPath { get; set; }
+            HelpText = "UE4 path to where files should get imported to")]
+        public string ImportedFilesPath { get; set; }
 
         [Option('e', "extension", Required = true,
-          HelpText = "File extensions to import.")]
-        public String FileExtensionToSearch { get; set; }        
+            HelpText = "File extensions to import.")]
+        public string FileExtensionToSearch { get; set; }
 
         [Option('c', "count", Required = true,
-          HelpText = "Amount of assets to process at once.")]
+            HelpText = "Amount of assets to process at once.")]
         public int AmountOfAssetsToProcess { get; set; }
 
         [Option('j', "json", Required = true,
-          HelpText = "Path to Import Settings JSON file.")]
+            HelpText = "Path to Import Settings JSON file.")]
         public string PathToImportJSON { get; set; }
 
         [Option('t', "tilesubfolder", Required = false,
-         HelpText = "If true, make subfolders for tiles.")]
+            HelpText = "If true, make subfolders for tiles.")]
         public bool MakeSubfolders { get; set; }
     }
-    class Program
-    {        
-        static void Main(string[] args)
+
+    internal class Program
+    {
+        private static void Main(string[] args)
         {
             // Start Progress Time now
-            DateTime startTime = DateTime.Now;
+            var startTime = DateTime.Now;
 
             // Get Command line args
-            var result = CommandLine.Parser.Default.ParseArguments<Options>(args);
+            var result = Parser.Default.ParseArguments<Options>(args);
 
             // If no errors occured..
-            if (!result.Errors.Any())
+            if (result.Errors.Any()) return;
+
+            // ..parse the JSON file.
+            var importSettingsJson = JObject.Parse(File.ReadAllText(result.Value.PathToImportJSON));
+
+            // Get refs to values we need to adjust later
+            var importFilesJson = (JArray) importSettingsJson.SelectToken("ImportGroups[0].FileNames");
+            var importDestinationJson = (JValue) importSettingsJson.SelectToken("ImportGroups[0].DestinationPath");
+
+            // Add Game prefix to path to bypass parsing bug with strings beginning with a forward slash
+            var finalDestination = "/Game/" + result.Value.ImportedFilesPath;
+            importDestinationJson.Value = finalDestination;
+
+            // Get files in source directory
+            var sourceDir = new DirectoryInfo(result.Value.SourceFilesPath);
+            var sourceFiles = sourceDir.GetFiles("*." + result.Value.FileExtensionToSearch);
+
+            // Get the file count and create a counter
+            var fileMaxCount = sourceFiles.Count();
+            var fileCount = 0;
+
+            // Create folders where processed files are being placed into
+            Directory.CreateDirectory(Path.Combine(result.Value.SourceFilesPath, "__tempimport"));
+            Directory.CreateDirectory(Path.Combine(result.Value.SourceFilesPath, "imported"));
+
+            // Get the folder the application is currently running in
+            var currentFolder = Environment.CurrentDirectory;
+
+            // Create new Progress Bar
+            using (var progress = new ProgressBar())
             {
-                // ..parse the JSON file.
-                JObject importSettingsJson = JObject.Parse(File.ReadAllText(result.Value.PathToImportJSON));
+                // Make array in which we will hold the files we will process
+                var processArray = new List<FileInfo>();
 
-                // Get refs to values we need to adjust later
-                JArray importGroupsJson = (JArray) importSettingsJson["ImportGroups"];
-                JArray importFilesJson = (JArray) importSettingsJson.SelectToken("ImportGroups[0].FileNames");
-                JValue importDestinationJson = (JValue)importSettingsJson.SelectToken("ImportGroups[0].DestinationPath");
-
-                // Add Game prefix to path to bypass parsing bug with strings beginning with a forward slash
-                string finalDestination = "/Game/" + result.Value.ImportedFilesPath;
-                importDestinationJson.Value = finalDestination;                                
-
-                // Get files in source directory
-                DirectoryInfo sourceDir = new DirectoryInfo(result.Value.SourceFilesPath);
-                FileInfo[] sourceFiles = sourceDir.GetFiles("*." + result.Value.FileExtensionToSearch);
-
-                // Get the file count and create a counter
-                int fileMaxCount = sourceFiles.Count();
-                int fileCount = 0;                                
-
-                // Create folders where processed files are being placed into
-                Directory.CreateDirectory(Path.Combine(result.Value.SourceFilesPath, "__tempimport"));
-                Directory.CreateDirectory(Path.Combine(result.Value.SourceFilesPath, "imported"));
-
-                // Get the folder the application is currently running in
-                var currentFolder = Environment.CurrentDirectory;
-
-                // Create new Progress Bar
-                using (var progress = new ProgressBar())
+                // For every file in the source folder..
+                foreach (var importFile in sourceFiles)
                 {
-                    // Make array in which we will hold the files we will process
-                    List<FileInfo> processArray = new List<FileInfo>();
+                    processArray.Add(importFile);
+                    // If we have the amount of files we want to process once, proceed..
+                    if (processArray.Count != result.Value.AmountOfAssetsToProcess) continue;
 
-                    // For every file in the source folder..
-                    foreach (FileInfo importFile in sourceFiles)
+                    // Move files from array into temp processing folder and add it to the JSON
+                    foreach (var tempImportFile in processArray)
                     {
-                        processArray.Add(importFile);
-                        // If we have the amount of files we want to process once, proceed..
-                        if (processArray.Count == result.Value.AmountOfAssetsToProcess)
-                        {        
-                            // Move files from array into temp processing folder and add it to the JSON
-                            foreach (FileInfo tempImportFile in processArray)
-                            {
-                                string tempNewFilePath = Path.Combine(tempImportFile.DirectoryName, "__tempimport", tempImportFile.Name);
-                                if (File.Exists(tempImportFile.FullName))
-                                {
-                                    File.Move(tempImportFile.FullName, tempNewFilePath);
-                                    importFilesJson.Add(tempNewFilePath);
-                                }
-                            }
+                        var tempNewFilePath = Path.Combine(tempImportFile.DirectoryName, "__tempimport",
+                            tempImportFile.Name);
+                        if (!File.Exists(tempImportFile.FullName)) continue;
 
-                            if (result.Value.MakeSubfolders)
-                            {
-                                string tileName = Path.GetFileNameWithoutExtension(importFile.FullName).Split(new[] { "d_" }, StringSplitOptions.None)[1];
-                                importDestinationJson.Value = finalDestination + "/" + tileName + "/";
-                            }                                
-
-                            // Write temp JSON file for importer
-                            File.WriteAllText(Path.Combine(currentFolder, "tempimport.json"), importSettingsJson.ToString());                          
-
-                            // Start a new process
-                            ProcessStartInfo psi = new ProcessStartInfo();
-                            psi.FileName = Path.Combine(result.Value.UE4Path, "UE4Editor-Cmd.exe");
-                            psi.WorkingDirectory = Path.GetDirectoryName(result.Value.UE4Path);
-                            psi.Arguments = "\"" + result.Value.ProjectPath + "\" -run=ImportAssets -importSettings=\"" + Path.Combine(currentFolder, "tempimport.json").Replace(@"\\", @"\") + "\" -AllowCommandletRendering";
-                            psi.CreateNoWindow = true;
-                            psi.RedirectStandardError = true;
-                            psi.RedirectStandardOutput = true;
-                            psi.UseShellExecute = false;
-
-                            // Get process output
-                            var process = Process.Start(psi);
-                            string processOutput = process.StandardOutput.ReadToEnd();
-
-                            // Wait for the process to end. Not using Event to block code execution.
-                            process.WaitForExit();
-
-                            // If the processing did not go without errors, log it.
-                            if (processOutput.Contains("Success - 0 error(s), 0 warning(s)") == false)
-                            {
-                                using (StreamWriter file = File.AppendText("error.txt"))
-                                {
-                                    string[] lines = processOutput.Split(
-                                        new[] { "\r\n", "\r", "\n" },
-                                        StringSplitOptions.None
-                                    );
-
-                                    foreach (string line in lines)
-                                        file.WriteLine(line);
-
-                                    file.WriteLine("---------------");                                    
-                                }
-                            }
-                            
-                            // Move imported files to imported folder.
-                            foreach (FileInfo tempImportFile in processArray)
-                            {
-                                string tempOldFilePath = Path.Combine(tempImportFile.DirectoryName, "__tempimport", tempImportFile.Name);
-                                string tempNewFilePath = Path.Combine(tempImportFile.DirectoryName, "imported", tempImportFile.Name);
-                                if (File.Exists(tempOldFilePath))                                                                    
-                                    File.Move(tempOldFilePath, tempNewFilePath);                                
-                            }                                                        
-
-                            // Update Progress
-                            fileCount += result.Value.AmountOfAssetsToProcess;
-                            TimeSpan timeRemaining = TimeSpan.FromTicks(DateTime.Now.Subtract(startTime).Ticks * (fileMaxCount - (fileCount + 1)) / (fileCount + 1));
-                            progress.timeRemainingText = timeRemaining.ToString("G");
-                            progress.Report((double)fileCount / fileMaxCount);
-
-                            processArray.Clear();
-                            importFilesJson.Clear();
-                        }                                             
+                        File.Move(tempImportFile.FullName, tempNewFilePath);
+                        importFilesJson.Add(tempNewFilePath);
                     }
-                }                
+
+                    if (result.Value.MakeSubfolders)
+                    {
+                        var tileName = Path.GetFileNameWithoutExtension(importFile.FullName)
+                            .Split(new[] {"d_"}, StringSplitOptions.None)[1];
+                        importDestinationJson.Value = finalDestination + "/" + tileName + "/";
+                    }
+
+                    // Write temp JSON file for importer
+                    File.WriteAllText(Path.Combine(currentFolder, "tempimport.json"),
+                        importSettingsJson.ToString());
+
+                    // Start a new process
+                    var psi = new ProcessStartInfo
+                    {
+                        FileName = Path.Combine(result.Value.UE4Path, "UE4Editor-Cmd.exe"),
+                        WorkingDirectory = Path.GetDirectoryName(result.Value.UE4Path) ?? throw new InvalidOperationException(),
+                        Arguments = "\"" + result.Value.ProjectPath +
+                                    "\" -run=ImportAssets -importSettings=\"" +
+                                    Path.Combine(currentFolder, "tempimport.json").Replace(@"\\", @"\") +
+                                    "\" -AllowCommandletRendering",
+                        CreateNoWindow = true,
+                        RedirectStandardError = true,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false
+                    };
+
+                    // Get process output
+                    var process = Process.Start(psi);
+                    var processOutput = process.StandardOutput.ReadToEnd();
+
+                    // Wait for the process to end. Not using Event to block code execution.
+                    process.WaitForExit();
+
+                    // If the processing did not go without errors, log it.
+                    if (processOutput.Contains("Success - 0 error(s), 0 warning(s)") == false)
+                        using (var file = File.AppendText("error.txt"))
+                        {
+                            var lines = processOutput.Split(
+                                new[] {"\r\n", "\r", "\n"},
+                                StringSplitOptions.None
+                            );
+
+                            foreach (var line in lines)
+                                file.WriteLine(line);
+
+                            file.WriteLine("---------------");
+                        }
+
+                    // Move imported files to imported folder.
+                    foreach (var tempImportFile in processArray)
+                    {
+                        var tempOldFilePath = Path.Combine(tempImportFile.DirectoryName, "__tempimport",
+                            tempImportFile.Name);
+                        var tempNewFilePath = Path.Combine(tempImportFile.DirectoryName, "imported",
+                            tempImportFile.Name);
+                        if (File.Exists(tempOldFilePath))
+                            File.Move(tempOldFilePath, tempNewFilePath);
+                    }
+
+                    // Update Progress
+                    fileCount += result.Value.AmountOfAssetsToProcess;
+                    var timeRemaining = TimeSpan.FromTicks(
+                        DateTime.Now.Subtract(startTime).Ticks * (fileMaxCount - (fileCount + 1)) /
+                        (fileCount + 1));
+                    progress.TimeRemainingText = timeRemaining.ToString("G");
+                    progress.Report((double) fileCount / fileMaxCount);
+
+                    processArray.Clear();
+                    importFilesJson.Clear();
+                }
             }
         }
     }
--- ProgressBar.cs
+++ ProgressBar.cs
@@ -2,33 +2,39 @@
 using System.Text;
 using System.Threading;
 
-namespace WoWUE4CmdImport
+namespace UnrealBatchImporter
 {
     public class ProgressBar : IDisposable, IProgress<double>
     {
-        private const int blockCount = 10;
-        private readonly TimeSpan animationInterval = TimeSpan.FromSeconds(1.0 / 8);
-        private const string animation = @"|/-\";
+        private const int BlockCount = 10;
+        private const string Animation = @"|/-\";
 
-        private readonly Timer timer;
+        private readonly TimeSpan _animationInterval = TimeSpan.FromSeconds(1.0 / 8);
+        private readonly Timer _timer;
 
-        private double currentProgress = 0;
-        private string currentText = string.Empty;
-        private bool disposed = false;
-        private int animationIndex = 0;
+        private int _animationIndex;
+        private double _currentProgress;
+        private string _currentText = string.Empty;
+        private bool _disposed;
 
-        public string timeRemainingText;
+        public string TimeRemainingText;
 
         public ProgressBar()
         {
-            timer = new Timer(TimerHandler);
+            _timer = new Timer(TimerHandler);
 
             // A progress bar is only for temporary display in a console window.
             // If the console output is redirected to a file, draw nothing.
             // Otherwise, we'll end up with a lot of garbage in the target file.
-            if (!Console.IsOutputRedirected)
+            if (!Console.IsOutputRedirected) ResetTimer();
+        }
+
+        public void Dispose()
+        {
+            lock (_timer)
             {
-                ResetTimer();
+                _disposed = true;
+                UpdateText(string.Empty);
             }
         }
 
@@ -36,22 +42,22 @@ namespace WoWUE4CmdImport
         {
             // Make sure value is in [0..1] range
             value = Math.Max(0, Math.Min(1, value));
-            Interlocked.Exchange(ref currentProgress, value);
+            Interlocked.Exchange(ref _currentProgress, value);
         }
 
         private void TimerHandler(object state)
         {
-            lock (timer)
+            lock (_timer)
             {
-                if (disposed) return;
+                if (_disposed) return;
 
-                int progressBlockCount = (int)(currentProgress * blockCount);
-                int percent = (int)(currentProgress * 100);
-                string text = string.Format("[{0}{1}] {2,3}% {3} - Time remaining: {4}",
-                    new string('#', progressBlockCount), new string('-', blockCount - progressBlockCount),
+                var progressBlockCount = (int) (_currentProgress * BlockCount);
+                var percent = (int) (_currentProgress * 100);
+                var text = string.Format("[{0}{1}] {2,3}% {3} - Time remaining: {4}",
+                    new string('#', progressBlockCount), new string('-', BlockCount - progressBlockCount),
                     percent,
-                    animation[animationIndex++ % animation.Length],
-                    timeRemainingText);
+                    Animation[_animationIndex++ % Animation.Length],
+                    TimeRemainingText);
 
                 UpdateText(text);
 
@@ -62,22 +68,20 @@ namespace WoWUE4CmdImport
         private void UpdateText(string text)
         {
             // Get length of common portion
-            int commonPrefixLength = 0;
-            int commonLength = Math.Min(currentText.Length, text.Length);
-            while (commonPrefixLength < commonLength && text[commonPrefixLength] == currentText[commonPrefixLength])
-            {
+            var commonPrefixLength = 0;
+            var commonLength = Math.Min(_currentText.Length, text.Length);
+            while (commonPrefixLength < commonLength && text[commonPrefixLength] == _currentText[commonPrefixLength])
                 commonPrefixLength++;
-            }
 
             // Backtrack to the first differing character
-            StringBuilder outputBuilder = new StringBuilder();
-            outputBuilder.Append('\b', currentText.Length - commonPrefixLength);
+            var outputBuilder = new StringBuilder();
+            outputBuilder.Append('\b', _currentText.Length - commonPrefixLength);
 
             // Output new suffix
             outputBuilder.Append(text.Substring(commonPrefixLength));
 
             // If the new text is shorter than the old one: delete overlapping characters
-            int overlapCount = currentText.Length - text.Length;
+            var overlapCount = _currentText.Length - text.Length;
             if (overlapCount > 0)
             {
                 outputBuilder.Append(' ', overlapCount);
@@ -85,22 +89,12 @@ namespace WoWUE4CmdImport
             }
 
             Console.Write(outputBuilder);
-            currentText = text;
+            _currentText = text;
         }
 
         private void ResetTimer()
         {
-            timer.Change(animationInterval, TimeSpan.FromMilliseconds(-1));
+            _timer.Change(_animationInterval, TimeSpan.FromMilliseconds(-1));
         }
-
-        public void Dispose()
-        {
-            lock (timer)
-            {
-                disposed = true;
-                UpdateText(string.Empty);
-            }
-        }
-
     }
 }
